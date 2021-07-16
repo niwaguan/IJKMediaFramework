334ae0626979dee6a069d20c8f7dde70f6f9c99f
niwaguan <niwaguan@gmail.com>
2021-07-16 22:06:00 +0800

    Initial Commit

---
+++ IJKMediaFramework/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  IJKMediaFramework
+//
+//  Created by Gaoyang on 2021/7/16.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ IJKMediaFramework/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  IJKMediaFramework
+//
+//  Created by Gaoyang on 2021/7/16.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
